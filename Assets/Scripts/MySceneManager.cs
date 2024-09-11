a5aeac298e5e8f5a1bac71f627d99a6992549694
Kinjen0 <118626914+Kinjen0@users.noreply.github.com>
2024-09-11 15:46:34 -0400

    Double check and fix issues with the XR interaction toolkit, and make sure it builds properly

---
+++ Assets/Scripts/MySceneManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/// <summary>
+/// This script is intended to allow the transitions between scenes, depending on a number of factors
+/// Currently:
+///     On Trigger Enter, I want to have the next scene load
+/// </summary>
+public class My_Scene_Manager : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            int curentScene = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(curentScene % SceneManager.sceneCountInBuildSettings);
+        }
+    }
+}
